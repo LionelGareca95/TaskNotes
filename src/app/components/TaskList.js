456098941b54bf70ad87527bff14062aa2aeb4a9
LionelGareca95 <lionelgareca@gmail.com>
2023-01-17 19:46:51 -0300

    proyecto terminado

--- src/app/components/TaskList.js
+++ src/app/components/TaskList.js
@@ -15,24 +15,40 @@ function TaskList(){
   return (
     <div className="w-4/6 ">
 
-        <header className="flex justify-beetwen items-center py-4">
+        <header className="flex justify-between items-center py-4">
             <h1>tasks {tasks.length}</h1>
-            <Link to='/create-task'>
+            <Link to='/create-task' className="bg-indigo-600 px-2 py-1 rounded-md text-md">
 
                 Create Task
 
             </Link>
         </header>
-        {
-            tasks.map(task => (
-                <div key={task.id}> 
-                    <h3>{task.title}</h3>
-                    <p>{task.description}</p>
-                    <button onClick={() => handleDelete(task.id)}>Delete</button>
-                    <Link to={`/edit-task/${task.id}`}>Edit</Link>
-                </div>
-            ))
-        }
+        <div className="grid grid-cols-3 gap-4">
+            {
+                tasks.map(task => (
+                    <div key={task.id} className="bg-neutral-800 p-4 rounded-md" > 
+                        <div className="flex justify-between">
+                            <h3>{task.title}</h3>
+                            <div className="flex gap-x-2">
+                                <Link to={`/edit-task/${task.id}`}
+                                    className="bg-zinc-600 px-2 py-1 text-sm rounded-md"
+                                >
+                                    Edit
+                                </Link>
+                                <button 
+                                    onClick={() => handleDelete(task.id)}
+                                    className="bg-red-500 px-2 py-1 text-sm rounded-md self-center">
+                                    Delete
+                                </button>
+                                
+                            </div>
+                        </div>                       
+                        <p>{task.description}</p>                   
+                    </div>
+                ))
+            }
+        </div>
+     
     </div>
   )
 }
