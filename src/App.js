2aac1023212da8c9ec8f377eb57ab3bce53ed71a
LionelGareca95 <lionelgareca@gmail.com>
2023-01-17 11:37:31 -0300

    save and delete task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 
 import './App.css';
-import { useSelector } from 'react-redux';
 import TaskForm from './app/components/TaskForm';
 import TaskList from './app/components/TaskList';
 
 function App() {
-
-  const tasksState = useSelector( state => state.tasks)
-  console.log(tasksState)
 
   return (
     <div className="App">
--- src/app/components/TaskList.js
+++ src/app/components/TaskList.js
@@ -1,8 +1,29 @@
+import { useSelector, useDispatch } from "react-redux"
+import { deleteTask } from "../../features/task/taskSlice"
 
-const taskList = () => {
+function TaskList(){
+
+   const tasks = useSelector( state => state.tasks)
+   const dispatch = useDispatch()
+   console.log(tasks)
+
+    const handleDelete = (id) => {
+        dispatch(deleteTask(id))
+    }
+
   return (
-    <div>taskList</div>
+    <div>
+        {
+            tasks.map(task => (
+                <div key={task.id}> 
+                    <h3>{task.title}</h3>
+                    <p>{task.description}</p>
+                    <button onClick={() => handleDelete(task.id)}>Delete</button>
+                </div>
+            ))
+        }
+    </div>
   )
 }
 
-export default taskList
+export default TaskList
