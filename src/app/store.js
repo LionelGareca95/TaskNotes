e892814a3fba2b35585ecd03162808557ff52d50
LionelGareca95 <lionelgareca@gmail.com>
2023-01-26 13:18:32 -0300

    añadiendo redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,39 @@
 import {configureStore} from '@reduxjs/toolkit'
 import tasksReducer from '../features/task/taskSlice'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers } from '@reduxjs/toolkit'
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+import taskSlice from '../features/task/taskSlice'
 
-export const store = configureStore( {
-    reducer: {
-        tasks: tasksReducer,
-    }
- 
+const reducers = combineReducers({
+    tasks: tasksReducer
+})
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers) 
+
+const store = configureStore( {
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 
 })
 
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import {store} from './app/store';
+import store from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
+let persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-       <App />
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        <App />
+      </PersistGate>    
     </Provider>
   </React.StrictMode>
 );
